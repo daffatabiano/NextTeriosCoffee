58ec690976fdede929a8f4ac1441db9da8337145
daffa tabiano <daffatabianoo@gmail.com>
2024-07-04 21:55:58 +0700

    modified(dashboard): fixing dashboard page layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,12 +16,14 @@ import {
 } from '@ant-design/icons';
 import Input from '../components/ui/Form/Input';
 import CountUp from 'react-countup';
+import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 
 const formatter: StatisticProps['formatter'] = (value) => (
     <CountUp end={value as number} separator="," />
 );
 
 export default function useDashboard() {
+    const { md } = useBreakpoint();
     const [collapsed, setCollapsed] = useState(false);
     const { Header, Sider, Content } = Layout;
     const {
@@ -86,38 +88,56 @@ export default function useDashboard() {
                     <h1 className="md:text-3xl text-2xl font-bold uppercase">
                         profile
                     </h1>
-                    <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-8">
-                        <div className="w-full my-4 flex justify-center items-center rounded-full ">
+                    <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0">
+                        <div className="w-full md:w-[25%] my-4 flex justify-center items-center rounded-full ">
                             <img
                                 className="w-[200px] h-[200px] p-2 object-cover object-center rounded-full bg-slate-200"
                                 src=""
                                 alt=""
                             />
                         </div>
-                        <div className="flex flex-col gap-4">
+                        <div className="flex flex-col w-full md:w-[50%] gap-4">
                             <Input
                                 placeholder="john doe"
                                 type="text"
                                 name="username"
                                 disabled
+                                style={{ cursor: 'not-allowed' }}
                             />
                             <Input
                                 placeholder="••••••••"
                                 type="password"
                                 name="password"
                                 disabled
+                                style={{ cursor: 'not-allowed' }}
                             />
                             <Input
                                 placeholder="email@mail.com"
                                 type="email"
                                 name="email"
                                 disabled
+                                style={{ cursor: 'not-allowed' }}
                             />
-                            <div className="flex justify-end">
+                            <div
+                                className="flex justify-end"
+                                style={{ cursor: 'pointer' }}
+                            >
                                 <EditOutlined />
                             </div>
                         </div>
-                        <Row gutter={16}>
+                        <Row
+                            gutter={!md ? 16 : 32}
+                            style={{
+                                display: 'flex',
+                                flexDirection: !md ? 'row' : 'column',
+                                justifyContent: !md
+                                    ? 'space-between'
+                                    : 'flex-start',
+                                alignItems: md ? 'center' : 'flex-start',
+                                width: md ? '25%' : '100%',
+                                textAlign: 'center',
+                            }}
+                        >
                             <Col>
                                 <Statistic
                                     title="MEMBER"
